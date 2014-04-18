5761b0625006f1e481a72b5dc7b34a3c9c35ba19
Ivan Calderon <iwano@hotmail.com>
2014-04-18 12:55:30 -0500

    Finish rations calculations table

--- app/assets/javascripts/angular/controllers/pregnant/rations_controller.js
+++ app/assets/javascripts/angular/controllers/pregnant/rations_controller.js
@@ -26,9 +26,19 @@
       totalCalories: 0
     };
 
-    $scope.rations = GeneralFormulas.rationsByGroup();
+    $scope.rationsValues = GeneralFormulas.rationsByGroup();
 
-    $scope.$watch('calories.totalCalories', function(newValues, oldValue) {
+    $scope.rations = GeneralFormulas.rationsTotalsByGroup();
+
+    $scope.rations.totalKcal = 1455;
+    $scope.rations.totalProteins = 80;
+    $scope.rations.totalLipids = 57;
+    $scope.rations.totalCarbohydrates = 158;
+
+    //Calories Calculations ***********************************
+    //*********************
+
+    $scope.$watch('calories.totalCalories', function(newValue, oldValue) {
       if ($scope.calories.proteins.percentage > 0) {
         $scope.calories.proteins.kcal = ($scope.calories.proteins.percentage * $scope.calories.totalCalories) / 100;
         $scope.calories.proteins.g = $scope.calories.proteins.kcal / 4;
@@ -58,6 +68,122 @@
 
     });
 
+    //Rations Calculations *************
+    //*********************
+
+    $scope.updateNutrimentsTotals = function(food, foodValues, newValue, oldValue) {
+      food.kcal = foodValues.kcal * newValue;
+      food.proteins = foodValues.proteins * newValue;
+      food.lipids = foodValues.lipids * newValue;
+      food.hc = foodValues.hc * newValue;
+
+      $scope.rations.totalKcal = $scope.rations.totalKcal - (oldValue * foodValues.kcal) + food.kcal;
+      $scope.rations.totalProteins = $scope.rations.totalProteins - (oldValue * foodValues.proteins) + food.proteins;
+      $scope.rations.totalLipids = $scope.rations.totalLipids - (oldValue * foodValues.lipids) + food.lipids;
+      $scope.rations.totalCarbohydrates = $scope.rations.totalCarbohydrates - (oldValue * foodValues.hc) + food.hc;
+    };
+
+    $scope.$watch('rations.vegetables.rations', function(newValue, oldValue) {
+      var fv = $scope.rationsValues.vegetables;
+      var f = $scope.rations.vegetables;
+      $scope.updateNutrimentsTotals(f, fv, newValue, oldValue);
+    });
+
+    $scope.$watch('rations.fruits.rations', function(newValue, oldValue) {
+      var fv = $scope.rationsValues.fruits;
+      var f = $scope.rations.fruits;
+      $scope.updateNutrimentsTotals(f, fv, newValue, oldValue);
+    });
+
+    $scope.$watch('rations.grains.without_fat.rations', function(newValue, oldValue) {
+      var fv = $scope.rationsValues.grains.without_fat;
+      var f = $scope.rations.grains.without_fat;
+      $scope.updateNutrimentsTotals(f, fv, newValue, oldValue);
+    });
+
+    $scope.$watch('rations.grains.with_fat.rations', function(newValue, oldValue) {
+      var fv = $scope.rationsValues.grains.with_fat;
+      var f = $scope.rations.grains.with_fat;
+      $scope.updateNutrimentsTotals(f, fv, newValue, oldValue);
+    });
+
+    $scope.$watch('rations.legumes.rations', function(newValue, oldValue) {
+      var fv = $scope.rationsValues.legumes;
+      var f = $scope.rations.legumes;
+      $scope.updateNutrimentsTotals(f, fv, newValue, oldValue);
+    });
+
+    $scope.$watch('rations.animal_source.very_low_fat.rations', function(newValue, oldValue) {
+      var fv = $scope.rationsValues.animal_source.very_low_fat;
+      var f = $scope.rations.animal_source.very_low_fat;
+      $scope.updateNutrimentsTotals(f, fv, newValue, oldValue);
+    });
+
+    $scope.$watch('rations.animal_source.low_fat.rations', function(newValue, oldValue) {
+      var fv = $scope.rationsValues.animal_source.low_fat;
+      var f = $scope.rations.animal_source.low_fat;
+      $scope.updateNutrimentsTotals(f, fv, newValue, oldValue);
+    });
+
+    $scope.$watch('rations.animal_source.moderated_fat.rations', function(newValue, oldValue) {
+      var fv = $scope.rationsValues.animal_source.moderated_fat;
+      var f = $scope.rations.animal_source.moderated_fat;
+      $scope.updateNutrimentsTotals(f, fv, newValue, oldValue);
+    });
+
+    $scope.$watch('rations.animal_source.high_fat.rations', function(newValue, oldValue) {
+      var fv = $scope.rationsValues.animal_source.high_fat;
+      var f = $scope.rations.animal_source.high_fat;
+      $scope.updateNutrimentsTotals(f, fv, newValue, oldValue);
+    });
+
+    $scope.$watch('rations.milk.skim.rations', function(newValue, oldValue) {
+      var fv = $scope.rationsValues.milk.skim;
+      var f = $scope.rations.milk.skim;
+      $scope.updateNutrimentsTotals(f, fv, newValue, oldValue);
+    });
+
+    $scope.$watch('rations.milk.semiskim.rations', function(newValue, oldValue) {
+      var fv = $scope.rationsValues.milk.semiskim;
+      var f = $scope.rations.milk.semiskim;
+      $scope.updateNutrimentsTotals(f, fv, newValue, oldValue);
+    });
+
+    $scope.$watch('rations.milk.whole.rations', function(newValue, oldValue) {
+      var fv = $scope.rationsValues.milk.whole;
+      var f = $scope.rations.milk.whole;
+      $scope.updateNutrimentsTotals(f, fv, newValue, oldValue);
+    });
+
+    $scope.$watch('rations.milk.sugary.rations', function(newValue, oldValue) {
+      var fv = $scope.rationsValues.milk.sugary;
+      var f = $scope.rations.milk.sugary;
+      $scope.updateNutrimentsTotals(f, fv, newValue, oldValue);
+    });
+
+    $scope.$watch('rations.fats_and_oils.without_proteins.rations', function(newValue, oldValue) {
+      var fv = $scope.rationsValues.fats_and_oils.without_proteins;
+      var f = $scope.rations.fats_and_oils.without_proteins;
+      $scope.updateNutrimentsTotals(f, fv, newValue, oldValue);
+    });
+
+    $scope.$watch('rations.fats_and_oils.with_proteins.rations', function(newValue, oldValue) {
+      var fv = $scope.rationsValues.fats_and_oils.with_proteins;
+      var f = $scope.rations.fats_and_oils.with_proteins;
+      $scope.updateNutrimentsTotals(f, fv, newValue, oldValue);
+    });
+
+    $scope.$watch('rations.sugars.without_fat.rations', function(newValue, oldValue) {
+      var fv = $scope.rationsValues.sugars.without_fat;
+      var f = $scope.rations.sugars.without_fat;
+      $scope.updateNutrimentsTotals(f, fv, newValue, oldValue);
+    });
+
+    $scope.$watch('rations.sugars.with_fat.rations', function(newValue, oldValue) {
+      var fv = $scope.rationsValues.sugars.with_fat;
+      var f = $scope.rations.sugars.with_fat;
+      $scope.updateNutrimentsTotals(f, fv, newValue, oldValue);
+    });
   }
 
 })();
--- app/assets/javascripts/angular/services/general_formulas.js
+++ app/assets/javascripts/angular/services/general_formulas.js
@@ -5,6 +5,130 @@
       rationsByGroup: function() {
         return {
           vegetables: {
+            kcal: 25,
+            proteins: 2,
+            lipids: 0,
+            hc: 4
+          },
+
+          fruits: {
+            kcal: 60,
+            proteins: 0,
+            lipids: 0,
+            hc: 15
+          },
+
+          grains: {
+            without_fat: {
+              kcal: 70,
+              proteins: 0,
+              lipids: 0,
+              hc: 15
+            },
+            with_fat: {
+              kcal: 115,
+              proteins: 2,
+              lipids: 5,
+              hc: 15
+            }
+          },
+
+          legumes: {
+            kcal: 120,
+            proteins: 8,
+            lipids: 1,
+            hc: 20
+          },
+
+          animal_source: {
+            very_low_fat: {
+              kcal: 40,
+              proteins: 7,
+              lipids: 1,
+              hc: 0
+            },
+            low_fat: {
+              kcal: 55,
+              proteins: 7,
+              lipids: 3,
+              hc: 0
+            },
+            moderated_fat: {
+              kcal: 75,
+              proteins: 7,
+              lipids: 5,
+              hc: 0
+            },
+            high_fat: {
+              kcal: 100,
+              proteins: 7,
+              lipids: 8,
+              hc: 0
+            },
+          },
+
+          milk: {
+            skim: {
+              kcal: 95,
+              proteins: 9,
+              lipids: 2,
+              hc: 12
+            },
+            semiskim: {
+              kcal: 110,
+              proteins: 9,
+              lipids: 4,
+              hc: 12
+            },
+            whole: {
+              kcal: 150,
+              proteins: 9,
+              lipids: 8,
+              hc: 12
+            },
+            sugary: {
+              kcal: 200,
+              proteins: 8,
+              lipids: 5,
+              hc: 30
+            },
+          },
+
+          fats_and_oils: {
+            without_proteins: {
+              kcal: 45,
+              proteins: 0,
+              lipids: 5,
+              hc: 15
+            },
+            with_proteins: {
+              kcal: 70,
+              proteins: 3,
+              lipids: 5,
+              hc: 3
+            }
+          },
+
+          sugars: {
+            without_fat: {
+              kcal: 40,
+              proteins: 0,
+              lipids: 0,
+              hc: 10
+            },
+            with_fat: {
+              kcal: 85,
+              proteins: 0,
+              lipids: 5,
+              hc: 10
+            }
+          }
+        }
+      },
+
+      rationsTotalsByGroup: function() {
+        return {
+          vegetables: {
             rations: 1,
             kcal: 25,
             proteins: 2,
