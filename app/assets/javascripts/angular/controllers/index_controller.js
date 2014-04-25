46ca919082f06ba05bbdc6a3e10f6486551a51fe
Ivan Calderon <iwano@hotmail.com>
2014-04-25 18:58:37 -0500

    WIP make it fullscreen

--- app/assets/javascripts/angular/controllers/index_controller.js
+++ app/assets/javascripts/angular/controllers/index_controller.js
@@ -7,6 +7,8 @@
 
   function controller($modal, $scope) {
 
+    $scope.sidebarToggle = false;
+
     $scope.openAboutModal = function() {
        $modal.open({
         templateUrl: 'angular/templates/about_modal.html',
