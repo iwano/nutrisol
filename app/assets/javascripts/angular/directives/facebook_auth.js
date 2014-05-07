3290c434c5aa8ece25a1d8c3b9ec20304011faf3
Ivan Calderon <iwano@hotmail.com>
2014-05-07 16:52:40 -0500

    Update facebook sdk script

--- app/assets/javascripts/angular/directives/facebook_auth.js
+++ app/assets/javascripts/angular/directives/facebook_auth.js
@@ -4,13 +4,13 @@ nutrisol.directive('facebook', ['$http', function($http) {
     scope: true,
     controller: ['$scope', '$attrs', function($scope, $attrs) {
       // Load the SDK Asynchronously
-      (function(d){
-        var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-        if (d.getElementById(id)) {return;}
-        js = d.createElement('script'); js.id = id; js.async = true;
-        js.src = "//connect.facebook.net/en_US/sdk.js";
-        ref.parentNode.insertBefore(js, ref);
-      }(document));
+      (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) return;
+         js = d.createElement(s); js.id = id;
+         js.src = "//connect.facebook.net/es_LA/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
 
       function login() {
         FB.login(function(response) {
