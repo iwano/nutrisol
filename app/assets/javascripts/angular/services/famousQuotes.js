a5b2673e6ea0cc8f3e290734bee6cdbdf2a1dcf0
Ivan Calderon <iwano@hotmail.com>
2014-05-10 15:32:44 -0500

    Format quotes prettier

--- app/assets/javascripts/angular/services/famousQuotes.js
+++ app/assets/javascripts/angular/services/famousQuotes.js
@@ -2,106 +2,106 @@
 
   var quotes = [
 
-    "“Early to bed and early to rise, makes a man healthy wealthy and wise”  ~Benjamin Franklin",
+    ["“Early to bed and early to rise, makes a man healthy wealthy and wise”", "~Benjamin Franklin"],
 
-    "“Physical fitness is not only one of the most important keys to a healthy body, it is the basis of dynamic and creative intellectual activity.”  ~John F. Kennedy",
+    ["“Physical fitness is not only one of the most important keys to a healthy body, it is the basis of dynamic and creative intellectual activity.”  ~John F. Kennedy"],
 
-    "“The greatest wealth is Health.”  ~Unknown",
+    ["“The greatest wealth is Health.”",  "~Unknown"],
 
-    "“Let food be thy medicine and medicine be thy food”  ~Hippocrates",
+    ["“Let food be thy medicine and medicine be thy food”", "~Hippocrates"],
 
-    "“Just because you’re not sick doesn’t mean you’re healthy”  ~Author Unknown",
+    ["“Just because you’re not sick doesn’t mean you’re healthy”", "~Author Unknown"],
 
-    "“Those who think they have no time for exercise will sooner or later have to find time for illness.”  ~Edward Stanley",
+    ["“Those who think they have no time for exercise will sooner or later have to find time for illness.”", "~Edward Stanley"],
 
-    "“If you don’t take care of your body, where are you going to live?”  ~Unknown",
+    ["“If you don’t take care of your body, where are you going to live?”", "~Unknown"],
 
-    "Healthy is merely the slowest rate at which one can die.  ~Unknown",
+    ["Healthy is merely the slowest rate at which one can die.", "~Unknown"],
 
-    "“Garbage in garbage out”  ~George Fuechsel",
+    ["“Garbage in garbage out”", "~George Fuechsel"],
 
-    "“Life expectancy would grow by leaps and bounds if green vegetables smelled as good as bacon.”  ~Doug Larson",
+    ["“Life expectancy would grow by leaps and bounds if green vegetables smelled as good as bacon.”", "~Doug Larson"],
 
 
-    "“Health is a state of complete harmony of the body, mind and spirit. When one is free from physical disabilities and mental distractions, the gates of the soul open.”  ~B.K.S. Iyengar",
+    ["“Health is a state of complete harmony of the body, mind and spirit. When one is free from physical disabilities and mental distractions, the gates of the soul open.”", "~B.K.S. Iyengar"],
 
-    "“If you can’t pronounce it, don’t eat it”  ~Common sense",
+    ["“If you can’t pronounce it, don’t eat it”", "~Common sense"],
 
-    "“In order to change we must be sick and tired of being sick and tired.”  ~Author Unknown",
+    ["“In order to change we must be sick and tired of being sick and tired.”", "~Author Unknown"],
 
-    "“Health is like money, we never have a true idea of its value until we lose it.”  ~Josh Billings",
+    ["“Health is like money, we never have a true idea of its value until we lose it.”", "~Josh Billings"],
 
-    "“Time And health are two precious assets that we don’t recognize and appreciate until they have been depleted.”  ~Denis Waitley",
+    ["“Time And health are two precious assets that we don’t recognize and appreciate until they have been depleted.”", "~Denis Waitley"],
 
-    "“From the bitterness of disease man learns the sweetness of health.”  ~Catalan Proverb",
+    ["“From the bitterness of disease man learns the sweetness of health.”", "~Catalan Proverb"],
 
-    "“Health and cheerfulness naturally beget each other.”  ~Joseph Addison",
+    ["“Health and cheerfulness naturally beget each other.”", "~Joseph Addison"],
 
-    "“Take care of your body. It’s the only place you have to live.”  ~Jim Rohn",
+    ["“Take care of your body. It’s the only place you have to live.”", "~Jim Rohn"],
 
-    "“Your body is a temple, but only if you treat it as one.”  ~Astrid Alauda",
+    ["“Your body is a temple, but only if you treat it as one.”", "~Astrid Alauda"],
 
-    "“Mainstream medicine would be way different if they focused on prevention even half as much as they focused on intervention…”  ~Anonymous",
+    ["“Mainstream medicine would be way different if they focused on prevention even half as much as they focused on intervention…”", "~Anonymous"],
 
-    "“Our bodies are our gardens – our wills are our gardeners.”  ~William Shakespeare",
+    ["“Our bodies are our gardens – our wills are our gardeners.”", "~William Shakespeare"],
 
-   "“The best and most efficient pharmacy is within your own system.”  ~Robert C. Peale",
+   ["“The best and most efficient pharmacy is within your own system.”", "~Robert C. Peale"],
 
-   "“Health is not simply the absence of sickness.”  ~Hannah Green",
+   ["“Health is not simply the absence of sickness.”", "~Hannah Green"],
 
-   "“My own prescription for health is less paperwork and more running barefoot through the grass”  ~Leslie Grimutter",
+   ["“My own prescription for health is less paperwork and more running barefoot through the grass”", "~Leslie Grimutter"],
 
-   "“The more you eat, the less flavor; the less you eat, the more flavor.”  ~Chinese Proverb",
+   ["“The more you eat, the less flavor; the less you eat, the more flavor.”", "~Chinese Proverb"],
 
-   "The best doctor gives the least medicine.",
+   ["The best doctor gives the least medicine.", ""],
 
-   "“The doctor of the future will no longer treat the human frame with drugs, but rather will cure and prevent disease with nutrition.”  ~Thomas Edison",
+   ["“The doctor of the future will no longer treat the human frame with drugs, but rather will cure and prevent disease with nutrition.”", "~Thomas Edison"],
 
-   "“An apple a day keeps the doctor away”  ~Proverb",
+   ["“An apple a day keeps the doctor away”", "~Proverb"],
 
-   "“A merry heart doeth good like a medicine, but a broken spirit dries the bones..”  ~Proverbs 17:22",
+   ["“A merry heart doeth good like a medicine, but a broken spirit dries the bones..”", "~Proverbs 17:22"],
 
-   "“True healthcare reform starts in your kitchen, not in Washington”  ~Anonymous",
+   ["“True healthcare reform starts in your kitchen, not in Washington”", "~Anonymous"],
 
-   "“The only way to keep your health is to eat what you don’t want, drink what you don’t like, and do what you’d rather not.”  ~Mark Twain",
+   ["“The only way to keep your health is to eat what you don’t want, drink what you don’t like, and do what you’d rather not.”", "~Mark Twain"],
 
-   "“To insure good health: eat lightly, breathe deeply, live moderately, cultivate cheerfulness, and maintain an interest in life.”  ~William Londen",
+   ["“To insure good health: eat lightly, breathe deeply, live moderately, cultivate cheerfulness, and maintain an interest in life.”", "~William Londen"],
 
-   "“A man too busy to take care of his health is like a mechanic too busy to take care of his tools.”  ~Spanish Proverb",
+   ["“A man too busy to take care of his health is like a mechanic too busy to take care of his tools.”", "~Spanish Proverb"],
 
-   "“To keep the body in good health is a duty, for otherwise we shall not be able to trim the lamp of wisdom, and keep our mind strong and clear. Water surrounds the lotus flower, but does not wet its petals.”  ~Buddha",
+   ["“To keep the body in good health is a duty, for otherwise we shall not be able to trim the lamp of wisdom, and keep our mind strong and clear. Water surrounds the lotus flower, but does not wet its petals.”", "~Buddha"],
 
-   "“Now there are more overweight people in America than average-weight people. So overweight people are now average. Which means you’ve met your New Year’s resolution.”  ~Jay Leno",
+   ["“Now there are more overweight people in America than average-weight people. So overweight people are now average. Which means you’ve met your New Year’s resolution.”", "~Jay Leno"],
 
-   "“Don’t eat anything your great-great grandmother wouldn’t recognize as food. There are a great many food-like items in the supermarket your ancestors wouldn’t recognize as food.. stay away from these”  ~Michael Pollan",
+   ["“Don’t eat anything your great-great grandmother wouldn’t recognize as food. There are a great many food-like items in the supermarket your ancestors wouldn’t recognize as food.. stay away from these”", "~Michael Pollan"],
 
-   "“Health is a relationship between you and your body”  ~Terri Guillemets",
+   ["“Health is a relationship between you and your body”", "~Terri Guillemets"],
 
-   "“He who takes medicine and neglects to diet wastes the skill of his doctors.”  ~Chinese Proverb",
+   ["“He who takes medicine and neglects to diet wastes the skill of his doctors.”", "~Chinese Proverb"],
 
-   "“Sickness comes on horseback but departs on foot.”  ~Dutch Proverb",
+   ["“Sickness comes on horseback but departs on foot.”", "~Dutch Proverb"],
 
-   "“Diseases of the soul are more dangerous and more numerous than those of the body.”  ~Cicero",
+   ["“Diseases of the soul are more dangerous and more numerous than those of the body.”", "~Cicero"],
 
-   "“A good laugh and a long sleep are the best cures in the doctor’s book.”  ~Irish Proverb",
+   ["“A good laugh and a long sleep are the best cures in the doctor’s book.”", "~Irish Proverb"],
 
-   "“Water, air and cleanliness are the chief articles in my pharmacopoeia.”  ~Napoleon I",
+   ["“Water, air and cleanliness are the chief articles in my pharmacopoeia.”", "~Napoleon I"],
 
-   "The 'i' in illness is isolation, and the crucial letters in wellness is 'we'.",
+   ["The 'i' in illness is isolation, and the crucial letters in wellness is 'we'.", ""],
 
-   "“Health of body and mind is a great blessing, if we can bear it.”   ~John Henry Cardinal Newman",
+   ["“Health of body and mind is a great blessing, if we can bear it.”", "~John Henry Cardinal Newman"],
 
-   "“Today, more than 95% of all chronic disease is caused by food choice, toxic food ingredients, nutritional deficiencies and lack of physical exercise.”  ~Mike Adams",
+   ["“Today, more than 95% of all chronic disease is caused by food choice, toxic food ingredients, nutritional deficiencies and lack of physical exercise.”", "~Mike Adams"],
 
-   "“It’s bizarre that the produce manager is more important to my children’s health than the pediatrician.”  ~Meryl Streep",
+   ["“It’s bizarre that the produce manager is more important to my children’s health than the pediatrician.”", "~Meryl Streep"],
 
-   "“Healing in a matter of time, but it is sometimes also a matter of opportunity.”  ~Hippocrates",
+   ["“Healing in a matter of time, but it is sometimes also a matter of opportunity.”", "~Hippocrates"],
 
-   "“The part can never be well unless the whole is well.”  ~Plato",
+   ["“The part can never be well unless the whole is well.”", "~Plato"],
 
-   "“Health is merely the slowest way someone can die.”  ~Author Unknown",
+   ["“Health is merely the slowest way someone can die.”", "~Author Unknown"],
 
-   "“By cleansing your body on a regular basis and eliminating as many toxins as possible from your environment, your body can begin to heal itself, prevent disease, and become stronger and more resilient than you ever dreamed possible!”  ~Dr. Edward Group III"
+   ["“By cleansing your body on a regular basis and eliminating as many toxins as possible from your environment, your body can begin to heal itself, prevent disease, and become stronger and more resilient than you ever dreamed possible!”", "~Dr. Edward Group III"]
   ]
 
   angular.module('services').constant('FamousQuotes', quotes);
