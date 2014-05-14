1066fbf133402dfe16a191ca5d40128552ad9413
Ivan Calderon <iwano@hotmail.com>
2014-05-14 16:01:13 -0500

    Set subsections li as active in sidebar

--- app/assets/javascripts/angular/routes.js
+++ app/assets/javascripts/angular/routes.js
@@ -9,9 +9,9 @@ nutrisol.config(['$stateProvider', '$urlRouterProvider',
 nutrisol.run(['$rootScope', '$state', 'FamousQuotes', function($rootScope, $state, FamousQuotes) {
   $rootScope.$on('$stateChangeSuccess',
     function(event, toState, toParams, fromState, fromParams){
-      $rootScope.currentSection = $state.current.name;
+      $rootScope.$state = $state;
       $rootScope.currentQuote = FamousQuotes[Math.floor((Math.random() * FamousQuotes.length))];
-      $rootScope.currentTemplateFolder = $rootScope.currentSection.split('.')[1];
+      $rootScope.currentTemplateFolder = $state.current.name.split('.')[1];
       $rootScope.sidebarToggle = false;
     });
 }]);
